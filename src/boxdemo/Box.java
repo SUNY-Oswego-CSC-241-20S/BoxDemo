5a30a041541cab64a83fa9c6e4ff2a8b5ddf4628
JamesPEarly <james.early@oswego.edu>
2023-02-03 07:49:27 -0500

    Reformatting

--- src/boxdemo/Box.java
+++ src/boxdemo/Box.java
@@ -1,23 +1,23 @@
 package boxdemo;
 
 public class Box {
-    // Instance variables
-    private String color;
-    private int numSides;
+  // Instance variables
+  private String color;
+  private int numSides;
 
-    // Constructor
-    public Box(String color, int numSides){
-        this.color = color;
-        this.numSides = numSides;
-    }
+  // Constructor
+  public Box(String color, int numSides) {
+    this.color = color;
+    this.numSides = numSides;
+  }
 
-    // "getter"
-    public String getColor(){
-        return color; // Instance variable value
-    }
+  // "getter"
+  public String getColor() {
+    return color; // Instance variable value
+  }
 
-    // "setter"
-    public void setColor(String color) {
-        this.color = color;
-    }
+  // "setter"
+  public void setColor(String color) {
+    this.color = color;
+  }
 }
